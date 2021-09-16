7d12b0d20f22d8a759b5f4708f762fef328c3666
keshavdulal <keshav.dulal@gmail.com>
2021-09-17 05:40:32 +0545

    Initial Commit

---
+++ 714-l02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  714-l02
+//
+//  Created by Keshav Dulal on 17/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
